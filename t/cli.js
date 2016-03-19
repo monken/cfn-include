283dae9619db1e48f117e3636f0054e8e370c64a
Moritz Onken <onken@netcubed.de>
2016-03-19 15:06:38 -0400

    cli tests

---
+++ t/cli.js
@@ -0,0 +1,21 @@
+var include = require('../index'),
+  assert = require('assert'),
+  fs = require('fs'),
+  exec = require('child_process').execFileSync;
+
+['cli'].forEach(function(file) {
+  var tests = require('./tests/' + file + '.json');
+  for (var category in tests) {
+    describe(category, function() {
+      tests[category].forEach(function(test) {
+        it(test.name || 'include', function(done) {
+          var out = exec('node', ['bin/cli.js', test.template]);
+          var json = JSON.parse(out.toString());
+          delete json.Metadata;
+          assert.deepEqual(json, test.output);
+          done();
+        });
+      });
+    });
+  }
+});
