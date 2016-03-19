33391dc42004a201bf300f807b90740b83324eeb
Moritz Onken <onken@netcubed.de>
2016-03-19 15:27:46 -0400

    execFileSync is only available in Node > 0.10 :-(

--- t/cli.js
+++ t/cli.js
@@ -1,7 +1,7 @@
 var include = require('../index'),
   assert = require('assert'),
   fs = require('fs'),
-  exec = require('child_process').execFileSync;
+  exec = require('child_process').execSync; // execFileSync is only available in Node > 0.10 :-(
 
 ['cli'].forEach(function(file) {
   var tests = require('./tests/' + file + '.json');
@@ -9,7 +9,7 @@ var include = require('../index'),
     describe(category, function() {
       tests[category].forEach(function(test) {
         it(test.name || 'include', function(done) {
-          var out = exec('node', ['bin/cli.js', test.template]);
+          var out = exec('node bin/cli.js ' + test.template);
           var json = JSON.parse(out.toString());
           delete json.Metadata;
           assert.deepEqual(json, test.output);
