1f7a42541f97dde495a6bdccda5ebdac72a27aeb
Moritz Onken <onken@netcubed.de>
2016-11-15 12:43:29 -0500

    catch body only if defined

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ function include(base, scope, args) {
       strictSSL: true,
     }).get('body').call('toString');
   }
-  body.catch(bail);
+  if(body) body.catch(bail);
   if (args.type === 'json') {
     return body.then(function(res) { return yaml.safeLoad(res, { schema: yamlSchema }) }).catch(function(yamlErr) {
       return body.then(jsmin).then(JSON.parse).catch(function(jsonErr) {
