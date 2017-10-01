0b6001d348fcc2e67e316fe3c0f5b69aa0e5fb4c
Moritz Onken <onken@netcubed.de>
2017-10-01 16:34:43 -0400

    remove bluebird dependency

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 var _ = require('lodash'),
   url = require('url'),
   path = require('path'),
-  Promise = require('bluebird'),
-  readFile = Promise.promisify(require('fs').readFile),
+  readFile = path => new Promise((resolve, reject) => require('fs').readFile(path, (err, data) => {
+    if (err) reject(err);
+    else resolve(data.toString());
+  })),
   pathParse = require('path-parse'),
-  request = Promise.promisify(require('request')),
+  requestFn = require('request'),
+  request = opts => new Promise((resolve, reject) => requestFn(opts, (err, res) => {
+    if (err) reject(err);
+    else resolve(res['body'].toString());
+  })),
+  p = require('./lib/promise'),
   AWS = require('aws-sdk-proxy'),
   s3 = new AWS.S3(),
   jsonlint = require('jsonlint'),
   yaml = require('./lib/yaml'),
   jmespath = require('jmespath');
-  parseLocation = require('./lib/parselocation');
+parseLocation = require('./lib/parselocation');
 
 
-module.exports = function(options) {
+module.exports = function (options) {
   var template = options.template,
     base = parseLocation(options.url),
     scope = options.scope || {};
   if (base.relative) throw "url cannot be relative";
   template = _.isUndefined(template) ? include(base, scope, options.url) : template;
-  return Promise.resolve(template).then(function(template) {
+  return Promise.resolve(template).then(function (template) {
     return recurse(base, scope, template);
   });
 }
@@ -34,35 +41,35 @@ function recurse(base, scope, object) {
         placeholder = args[1],
         body = args[args.length - 1];
       if (args.length === 2) placeholder = '_';
-      return recurse(base, scope, list).map(function(replace) {
+      return p.map(recurse(base, scope, list), function (replace) {
         scope = _.clone(scope);
         scope[placeholder] = replace;
         var replaced = findAndReplace(scope, _.cloneDeep(body));
         return recurse(base, scope, replaced);
       });
     } else if (object["Fn::Include"]) {
-      return include(base, scope, object["Fn::Include"]).then(function(json) {
-        if(!_.isPlainObject(json)) return json;
+      return include(base, scope, object["Fn::Include"]).then(function (json) {
+        if (!_.isPlainObject(json)) return json;
         delete object["Fn::Include"];
         _.defaults(object, json);
         return object;
       }).then(_.bind(findAndReplace, this, scope)).then(_.bind(recurse, this, base, scope));
     } else if (object["Fn::Flatten"]) {
-      return recurse(base, scope, object["Fn::Flatten"]).then(function(json) {
+      return recurse(base, scope, object["Fn::Flatten"]).then(function (json) {
         return _.flatten(json);
       });
     } else if (object["Fn::Merge"]) {
-      return recurse(base, scope, object["Fn::Merge"]).then(function(json) {
+      return recurse(base, scope, object["Fn::Merge"]).then(function (json) {
         return _.merge.apply(_, json);
       });
     } else if (object["Fn::Stringify"]) {
-      return recurse(base, scope, object["Fn::Stringify"]).then(function(json) {
+      return recurse(base, scope, object["Fn::Stringify"]).then(function (json) {
         return JSON.stringify(json);
       });
     } else {
-      return Promise.props(_.mapValues(object, _.bind(recurse, this, base, scope)))
+      return p.props(_.mapValues(object, _.bind(recurse, this, base, scope)))
     }
-  } else if(_.isUndefined(object)) {
+  } else if (_.isUndefined(object)) {
     return null;
   } else {
     return object;
@@ -70,7 +77,7 @@ function recurse(base, scope, object) {
 }
 
 function findAndReplace(scope, object) {
-  _.forEach(scope, function(replace, find) {
+  _.forEach(scope, function (replace, find) {
     var regex = new RegExp('\\${' + find + '}', 'g');
     if (_.isString(object) && object === find) {
       object = replace;
@@ -79,10 +86,10 @@ function findAndReplace(scope, object) {
     } else if (_.isArray(object)) {
       object = object.map(_.bind(findAndReplace, this, scope));
     } else if (_.isPlainObject(object)) {
-      object = _.mapKeys(object, function(value, key) {
+      object = _.mapKeys(object, function (value, key) {
         return findAndReplace(scope, key);
       });
-      _.keys(object).forEach(function(key) {
+      _.keys(object).forEach(function (key) {
         if (key === 'Fn::Map') return;
         object[key] = findAndReplace(scope, object[key]);
       });
@@ -96,9 +103,9 @@ function findAndReplace(scope, object) {
 }
 
 function interpolate(lines, context) {
-  return lines.map(function(line) {
+  return lines.map(function (line) {
     var parts = [];
-    line.split(/({{\w+?}})/g).map(function(line) {
+    line.split(/({{\w+?}})/g).map(function (line) {
       var match = line.match(/^{{(\w+)}}$/),
         value = match ? context[match[1]] : undefined;
       if (!match) return line;
@@ -107,7 +114,7 @@ function interpolate(lines, context) {
       } else {
         return value;
       }
-    }).forEach(function(part) {
+    }).forEach(function (part) {
       var last = parts[parts.length - 1];
       if (_.isPlainObject(part) || _.isPlainObject(last) || !parts.length) {
         parts.push(part);
@@ -115,7 +122,7 @@ function interpolate(lines, context) {
         parts[parts.length - 1] = last + part;
       }
     });
-    return parts.filter(function(part) {
+    return parts.filter(function (part) {
       return part !== '';
     });
   });
@@ -129,7 +136,7 @@ function include(base, scope, args) {
   if (!_.isEmpty(location) && !location.protocol) location.protocol = base.protocol;
   if (location.protocol === 'file') {
     absolute = location.relative ? path.join(path.dirname(base.path), location.host, location.path || '') : [location.host, location.path].join('');
-    body = readFile(absolute).call('toString');
+    body = readFile(absolute);
     absolute = location.protocol + '://' + absolute;
   } else if (location.protocol === 's3') {
     var basedir = pathParse(base.path).dir;
@@ -137,10 +144,10 @@ function include(base, scope, args) {
       key = location.relative ? url.resolve(basedir + '/', location.raw) : location.path;
     key = key.replace(/^\//, '');
     absolute = location.protocol + '://' + [bucket, key].join('/');
-    body = Promise.promisify(s3.getObject).call(s3, {
+    body = s3.getObject({
       Bucket: bucket,
       Key: key,
-    }).get('Body').call('toString');
+    }).promise().then(res => res['Body'].toString());
   } else if (location.protocol && location.protocol.match(/^https?$/)) {
     var basepath = pathParse(base.path).dir + '/';
     absolute = location.relative ? url.resolve(location.protocol + '://' + base.host + basepath, location.raw) : location.raw;
@@ -149,10 +156,10 @@ function include(base, scope, args) {
       followRedirect: false,
       gzip: true,
       strictSSL: true,
-    }).get('body').call('toString');
+    });
   }
   if (args.type === 'json') {
-    return body.then(yaml.load).then(function(template) {
+    return body.then(yaml.load).then(function (template) {
       if (args.query) {
         template = jmespath.search(template, args.query);
       }
@@ -164,7 +171,7 @@ function include(base, scope, args) {
   } else if (args.type === 'string') {
     return body;
   } else if (args.type === 'literal') {
-    return body.then(function(template) {
+    return body.then(function (template) {
       var lines = JSONifyString(template);
       if (_.isPlainObject(args.context)) {
         lines = interpolate(lines, args.context);
@@ -179,7 +186,7 @@ function include(base, scope, args) {
 function JSONifyString(string) {
   var lines = [],
     split = string.toString().split(/(\r?\n)/);
-  split.forEach(function(line, idx) {
+  split.forEach(function (line, idx) {
     if (idx % 2) {
       lines[(idx - 1) / 2] = lines[(idx - 1) / 2] + line;
     } else {
